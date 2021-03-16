e0b0a40dc504d17af2d3cd3ba5556a73dc0fa842
huiminlim <e0036379@u.nus.edu>
2021-03-16 18:48:49 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 # Declare an array of string with type
-declare -a StringArray=("README.adoc" "01-front_pitch.adoc" "02-development_setup.adoc")
+declare -a StringArray=("index.adoc" "01-front_pitch.adoc" "02-development_setup.adoc")
  
  
 # Iterate the string array using for loop
