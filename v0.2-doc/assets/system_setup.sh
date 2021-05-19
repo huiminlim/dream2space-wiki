cb15f8b3e601d4d063835201de456b47c50a2e20
huiminlim <e0036379@u.nus.edu>
2021-05-19 21:48:41 +0800

    debug shell scripts for payload setup

--- v0.2-doc/assets/system_setup.sh
+++ v0.2-doc/assets/system_setup.sh
@@ -3,7 +3,11 @@
 # Source: https://gist.github.com/damoclark/ab3d700aafa140efb97e510650d9b1be
 
 echo "Running automated tasks to mass produce d2s Cubesat Payload Manager SD card"
-
+for (( i=5; i>0; i--)); do
+  sleep 1 &
+  printf "Starting in $i ...\r"
+  wait
+done
 
 # Don't change the following lines unless you know what you are doing
 # They execute the config options starting with 'do_' below
@@ -29,7 +33,6 @@ do_blanking             1       # Disable blanking of screen due to inactivity
 do_camera               0       # Enable camera
 do_serial               0       # Enable serial console
 do_serial               2       # Disable serial console, but enable serial hardware (/dev/serial0)
-do_ssh                  0       # Enable remote ssh login
 do_vnc                  0       # Enable VNC
 
 # Don't add any raspi-config configuration options after 'END' line below & don't remove 'END' line
